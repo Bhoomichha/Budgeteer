b65ed54e63cfc9079d0f0527578cfdaa88b33567
Bhoomichha <bdangol@my.centennialcollege.ca>
2023-11-05 20:13:00 -0500

    Connecting DB and middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { db } = require('./db/db.js'); 
 
 require('dotenv').config()
 
@@ -9,10 +10,11 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res)=> {
-    
+  res.send('Hello World')  
 })
 const PORT = process.env.PORT
 const server = () => {
+  db()
     app.listen(PORT, () =>{
         console.log(`Server is running on port ${PORT}`)    
     })
