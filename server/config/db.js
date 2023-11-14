74ab2dedaabbdde97ebff8179bf829fb1a0621bc
Bhoomichha <bdangol@my.centennialcollege.ca>
2023-11-14 16:12:35 -0500

    removing the exposure of mongoDB url

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ mongoose.set('strictQuery', false);
 const db = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL)
-        console.log(`DB Connected: ${conn.connection.host}`);
+        console.log(`Database Connected!`);
     } catch (error) {
         console.log('Connection Error');
     }
